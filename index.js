2b6c3630db39f2c531c15c948b58303c626a1169
AmasiaNalbandian <amasia.nalbandian@mitel.com>
2021-09-30 01:06:25 -0400

    refactored code to reuse for files/directories and ensures all lines have p tag.

--- index.js
+++ index.js
@@ -98,7 +98,12 @@ if(options.index){
         })();
 
       }
-      createHtml();
+      let extension = path.extname(process.argv[3]);
+      if (extension  === ".txt") {
+        createHtml();
+      } else if (extension === ".md") {
+        readMarkdownFile(process.argv[3], "dist")
+      }
     
   } else if (isDir) {
 
@@ -166,8 +171,8 @@ if(options.index){
       
       
           })
-        }  else if (path.extname(file) == ".md") {
-          const folderName = "dir";
+        }else if (path.extname(file) == ".md") {
+          const folderName = "dist";
   
           const htmlFile = fs.readFileSync(`${__dirname}/index.html`);
   
@@ -177,42 +182,8 @@ if(options.index){
             `${process.cwd()}/${folderName}/${filenameWithoutExt}.html`,
             htmlFile
           );
-  
-          fs.readFile(
-            file,
-            { encoding: "utf8", flag: "r" },
-            function (err, data) {
-              if (err) console.log(err);
-              let newData = "";
-              let title = removeMarkdownFormatting(data.split("\n")[0]);
-              data = data.replace(title, ""); // removes the title from the text
-  
-              var editedText = data
-                .split(/\r?\n\r?\n/)
-                .map((para) => <p style=" font-family: 'Gentium Basic', serif; font-size: 24px; padding: 10px; border-radius: 20px"> + createHTMLFromMarkdown(para) + </p>)
-                .join("\n");
-  
-              titleInsidePTag = `<h1 style="text-align: center; background-color: black; color: white; width: 50%; height: 100%; border-radius: 10px; margin: auto; top: 15px; ">${title}</h1>`;
-  
-              // Appending the title
-              fs.appendFile(
-                `${process.cwd()}/${folderName}/${path.parse(file).name}.html`,
-                titleInsidePTag,
-                function (err) {
-                  if (err) throw err;
-                }
-              );
-  
-              // Appending the rest of the text
-              fs.appendFile(
-                `${process.cwd()}/${folderName}/${path.parse(file).name}.html`,
-                editedText.replace(title, ""),
-                function (err) {
-                  if (err) throw err;
-                }
-              );
-            }
-          );
+            
+            readMarkdownFile(file, folderName)
         }
       })
 
@@ -223,7 +194,7 @@ if(options.index){
 }
 
 function createHTMLFromMarkdown(para) {
-  return  para
+  let p =  para
     .replace(/^# (.*$)/gim, "<h1>$1</h1>")
     .replace(/^## (.*$)/gim, "<h2>$1</h2>")
     .replace(/^--- (.*$)/gim, "<hr/>")
@@ -233,6 +204,8 @@ function createHTMLFromMarkdown(para) {
 		.replace(/\*(.*)\*/gim, '<i>$1</i>')
 		.replace(/\[(.*?)\]\((.*?)\)/gim, "<a href='$2'>$1</a>")
 		.replace(/\n$/gim, '<br /><br />');
+
+    return `<p style=" font-family: 'Gentium Basic', serif; font-size: 24px; padding: 10px; border-radius: 20px">${p}</p>`;
 }
 
 function removeMarkdownFormatting(text){
@@ -244,6 +217,43 @@ function removeMarkdownFormatting(text){
 		.replace(/\*\*(.*)\*\*/gim, '$1')
 		.replace(/\*(.*)\*/gim, '$1')
 		.replace(/\n$/gim, '$1')
+}
+
+function readMarkdownFile(file, folderName) {
+  fs.readFile(
+    file,
+    { encoding: "utf8", flag: "r" },
+    function (err, data) {
+      if (err) console.log(err);
+      let title = removeMarkdownFormatting(data.split("\n")[0]);
+      data = data.replace(title, ""); // removes the title from the text
+
+      var editedText = data
+        .split(/\r?\n\r?\n/)
+        .map((para) => (createHTMLFromMarkdown(para)))
+        .join("\n");
+
+      let titleInsidePTag = `<h1 style="text-align: center; background-color: black; color: white; width: 50%; border-radius: 10px; margin: auto; top: 15px; ">${title}</h1>`;
+      
+      // Appending the title
+      fs.appendFile(
+        `${process.cwd()}/${folderName}/${path.parse(file).name}.html`,
+        titleInsidePTag,
+        function (err) {
+          if (err) throw err;
+        }
+      );
+
+      // Appending the rest of the text
+      fs.appendFile(
+        `${process.cwd()}/${folderName}/${path.parse(file).name}.html`,
+        editedText.replace(title, ""),
+        function (err) {
+          if (err) throw err;
+        }
+      );
+    }
+  );
 }
 
 // ----------------------------------------------------------------------------------------------------------
