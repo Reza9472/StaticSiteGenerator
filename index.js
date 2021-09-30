2e45f5126bfda9533f712f4b18c382d3bce9dad6
AmasiaNalbandian <amasia.nalbandian@mitel.com>
2021-09-30 00:03:30 -0400

    pulled code from main, and re-added md

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 #! /usr/bin/env node
+
 const fs = require('fs');
 const path = require('path');
 const { createInterface } = require('readline');
@@ -8,81 +9,76 @@ const { program } = require('commander');
 
 
 program.version( require('./package.json').version); // Getting the verison of the file
-program.option("-i ,--index", "Single txt file "); // option for adding a single text file
-// .option('-i, --folder', 'All text files from the "Text Files" folder ') // option for adding a folder
+program.option('-i ,--index', 'Single txt file ') // option for adding a single text file
+
 
 program.parse(process.argv);
 
 const options = program.opts(); // The --index or -i options
 
-if (options.index) {
+if(options.index){
   let stats = fs.statSync(process.argv[3]); // Finding out if the file is a file or folder
 
-  let isFile = stats.isFile();
-  let isDir = stats.isDirectory();
+  let isFile = stats.isFile() 
+  let isDir = stats.isDirectory()
+  
+  if (isFile) { // if the passed value is a file
 
-  if (isFile) {
-    // if the passed value is a file
+    const folderName = 'dist'; 
 
-    // console.log(process.argv[3]);
-
-    const folderName = "dir";
-
-    // creating the dir folder if it doesn't exist
+    // creating the dist folder if it doesn't exist
 
     try {
       if (!fs.existsSync(folderName)) {
-        fs.mkdirSync(folderName);
+        fs.mkdirSync(folderName)
       }
     } catch (err) {
       console.error(err);
     }
 
-    function createHtml() {
-      const htmlFile = fs.readFileSync(`${__dirname}/index.html`);
-      var filename = process.argv[3]; // getting the filename
+    function createHtml(){
 
-      filenameWithoutExt = path.parse(filename).name; // The name part of the filename without thwe extension
+        const htmlFile = fs.readFileSync(`${__dirname}/index.html`)
+        var filename = process.argv[3]; // getting the filename
 
-      fs.writeFileSync(
-        `${process.cwd()}/${folderName}/${filenameWithoutExt}.html`,
-        htmlFile
-      );
+        filenameWithoutExt = path.parse(filename).name; // The name part of the filename without thwe extension
+        
+        fs.writeFileSync(`${process.cwd()}/${folderName}/${filenameWithoutExt}.html` , htmlFile);
 
-      let counter = true; // for skipping the header
-      (async function processLineByLine() {
-        try {
-          const rl = createInterface({
-            input: createReadStream(filename),
-            crlfDelay: Infinity,
-          });
 
-          let skipHeader = true; // for getting the header when we append it to the html
-          rl.on("line", (line) => {
-            if (counter === true) {
-              // console.log(line);
-              let header = `<h1 style='text-align: center; background-color: black; color: white; width: 50%; height: 100%; border-radius: 10px; margin: auto;'>${line}</h1>`;
+        let counter = true; // for skipping the header 
+        (async function processLineByLine() {
+          try {
+            const rl = createInterface({
+              input: createReadStream(filename),
+              crlfDelay: Infinity
+            });
+            
+            let skipHeader = true; // for getting the header when we append it to the html
+            rl.on('line', (line) => {
 
-              fs.appendFile(
-                `${process.cwd()}/${folderName}/${filenameWithoutExt}.html`,
-                header,
-                function (err) {
-                  if (err) throw err;
-                }
-              );
-            }
-            counter = false;
+              if(counter === true){
 
-            if (line !== "") {
-              var toPrepand = `<p style="text-align: center;  font-family: 'Gentium Basic', serif; font-size: 20px; ">`;
+                let header = `<h1 style='text-align: center; background-color: black; color: white; width: 50%; height: 100%; border-radius: 10px; margin: auto;'>${line}</h1>`;
 
-              toPrepand = toPrepand.concat(`${line}`);
-            } else {
-              var toPrepand = "</p><br>";
-            }
+                fs.appendFile(`${process.cwd()}/${folderName}/${filenameWithoutExt}.html` , header , function(err){
+                  if(err) throw err;
+                })
+              } 
+              counter = false;
 
+
+              if(line !== ''){
+
+                var toPrepand = `<p style="text-align: center;  font-family: 'Gentium Basic', serif; font-size: 20px; ">`;
+                
+                toPrepand = toPrepand.concat(`${line}`);
+              }else{
+                var toPrepand = '</p><br>';
+              }
 
               // Process the line.
+
               if(skipHeader === false){
 
                 fs.appendFile(`${process.cwd()}/${folderName}/${filenameWithoutExt}.html` , toPrepand , function(err){
@@ -170,150 +166,61 @@ if (options.index) {
       
       
           })
+        }  else if (path.extname(file) == ".md") {
+          const folderName = "dir";
+  
+          const htmlFile = fs.readFileSync(`${__dirname}/index.html`);
+  
+          filenameWithoutExt = path.parse(file).name; // The name part of the file (EX: name.txt => name)
+  
+          fs.writeFileSync(
+            `${process.cwd()}/${folderName}/${filenameWithoutExt}.html`,
+            htmlFile
+          );
+  
+          fs.readFile(
+            file,
+            { encoding: "utf8", flag: "r" },
+            function (err, data) {
+              if (err) console.log(err);
+              let newData = "";
+              let title = removeMarkdownFormatting(data.split("\n")[0]);
+              data = data.replace(title, ""); // removes the title from the text
+  
+              var editedText = data
+                .split(/\r?\n\r?\n/)
+                .map((para) => (createHTMLFromMarkdown(para)))
+                .join("\n");
+  
+              titleInsidePTag = `<h1 style="text-align: center; background-color: black; color: white; width: 50%; height: 100%; border-radius: 10px; margin: auto; top: 15px; ">${title}</h1>`;
+  
+              // Appending the title
+              fs.appendFile(
+                `${process.cwd()}/${folderName}/${path.parse(file).name}.html`,
+                titleInsidePTag,
+                function (err) {
+                  if (err) throw err;
+                }
+              );
+  
+              // Appending the rest of the text
+              fs.appendFile(
+                `${process.cwd()}/${folderName}/${path.parse(file).name}.html`,
+                editedText.replace(title, ""),
+                function (err) {
+                  if (err) throw err;
+                }
+              );
+            }
+          );
         }
       })
 
-    console.log(process.argv[3]);
-
-    // Function to get current filenames
-    // in directory with specific extension
-    files = fs.readdirSync(__dirname + "/text files");
-    // console.log(path.extname(process.argv[3]));
-
-    files.forEach((file) => {
-      // getting the files inside the folder
-
-      // console.log("\Filenames with the .txt extension:");
-      if (path.extname(file) == ".txt") {
-        // Finding the text files
-        const folderName = "dir";
-
-        const htmlFile = fs.readFileSync(`${__dirname}/index.html`);
-
-        filenameWithoutExt = path.parse(file).name; // The name part of the file (EX: name.txt => name)
-
-        fs.writeFileSync(
-          `${process.cwd()}/${folderName}/${filenameWithoutExt}.html`,
-          htmlFile
-        );
-
-        fs.readFile(
-          file,
-          { encoding: "utf8", flag: "r" },
-          function (err, data) {
-            if (err) console.log(err);
-            else
-              var editedText = data // Editing the text to recieved from the files
-                .split(/\r?\n\r?\n/)
-                .map(
-                  (para) =>
-                    `<p style="text-align: center; margin: 60px; font-family: 'Gentium Basic', serif; font-size: 24px; background-color: #fff2cc; padding: 10px; border-radius: 20px">${para.replace(
-                      /\r?\n/,
-                      " "
-                    )}</p>`
-                )
-                .join(" ");
-
-            let title = editedText.split("</p>")[0].split(">", 2)[1]; // getting the title of the text
-
-            titleInsidePTag = `<h1 style="text-align: center; background-color: black; color: white; width: 50%; height: 100%; border-radius: 10px; margin: auto; top: 15px; ">${title}</h1>`;
-
-            // Appending the title
-            fs.appendFile(
-              `${process.cwd()}/${folderName}/${path.parse(file).name}.html`,
-              titleInsidePTag,
-              function (err) {
-                if (err) throw err;
-              }
-            );
-
-            // Appending the rest of the text
-            fs.appendFile(
-              `${process.cwd()}/${folderName}/${path.parse(file).name}.html`,
-              editedText.replace(title, ""),
-              function (err) {
-                if (err) throw err;
-              }
-            );
-          }
-        );
-      } else if (path.extname(file) == ".md") {
-        const folderName = "dir";
-
-        const htmlFile = fs.readFileSync(`${__dirname}/index.html`);
-
-        filenameWithoutExt = path.parse(file).name; // The name part of the file (EX: name.txt => name)
-
-        fs.writeFileSync(
-          `${process.cwd()}/${folderName}/${filenameWithoutExt}.html`,
-          htmlFile
-        );
-
-        fs.readFile(
-          file,
-          { encoding: "utf8", flag: "r" },
-          function (err, data) {
-            if (err) console.log(err);
-            let newData = "";
-            let title = removeMarkdownFormatting(data.split("\n")[0]);
-            data = data.replace(title, ""); // removes the title from the text
-
-            var editedText = data
-              .split(/\r?\n\r?\n/)
-              .map((para) => (createHTMLFromMarkdown(para)))
-              .join("\n");
-
-            titleInsidePTag = `<h1 style="text-align: center; background-color: black; color: white; width: 50%; height: 100%; border-radius: 10px; margin: auto; top: 15px; ">${title}</h1>`;
-
-            // Appending the title
-            fs.appendFile(
-              `${process.cwd()}/${folderName}/${path.parse(file).name}.html`,
-              titleInsidePTag,
-              function (err) {
-                if (err) throw err;
-              }
-            );
-
-            // Appending the rest of the text
-            fs.appendFile(
-              `${process.cwd()}/${folderName}/${path.parse(file).name}.html`,
-              editedText.replace(title, ""),
-              function (err) {
-                if (err) throw err;
-              }
-            );
-          }
-        );
-      }
-    });
-
-    console.log("Files processed.");
-    console.log("HTML Files created.");
+          console.log('Files processed.');
+          console.log('HTML Files created.');
   }
-}
-
-function createHTMLFromMarkdown(para) {
-  return  para
-    .replace(/^# (.*$)/gim, "<h1>$1</h1>")
-    .replace(/^## (.*$)/gim, "<h2>$1</h2>")
-    .replace(/^--- (.*$)/gim, "<hr/>")
-    .replace(/^### (.*$)/gim, '<h3>$1</h3>')
-		.replace(/^\> (.*$)/gim, '<blockquote>$1</blockquote>')
-		.replace(/\*\*(.*)\*\*/gim, '<b>$1</b>')
-		.replace(/\*(.*)\*/gim, '<i>$1</i>')
-		.replace(/\[(.*?)\]\((.*?)\)/gim, "<a href='$2'>$1</a>")
-		.replace(/\n$/gim, '<br /><br />');
-}
-
-function removeMarkdownFormatting(text){
-  return text
-		.replace(/^### (.*$)/gim, '$1')
-		.replace(/^## (.*$)/gim, '$1')
-		.replace(/^# (.*$)/gim, '$1')
-		.replace(/^\> (.*$)/gim, '$1')
-		.replace(/\*\*(.*)\*\*/gim, '$1')
-		.replace(/\*(.*)\*/gim, '$1')
-		.replace(/\n$/gim, '$1')
+ 
 }
 
 // ----------------------------------------------------------------------------------------------------------
+
